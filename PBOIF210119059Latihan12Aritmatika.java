1291ab3500acd2b0d6c0853949115aca10ab10ef
irgidp <irgijak@gmail.com>
2020-10-15 22:23:50 +0700

    Latihan 12

---
+++ PBOIF210119059Latihan12Aritmatika.java
@@ -0,0 +1,35 @@
+/* 
+* @author
+ * NAMA        : Irgi Dwiputra
+ * KELAS       : IF-2
+ * NIM         : 10119059
+ * Deskripsi   : Program ini berisi program untuk menampilkan penulisan artimatika
+ */
+package pboif2.pkg10119059.latihan12.aritmatika;
+
+public class PBOIF210119059Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+    }
+    
+}
